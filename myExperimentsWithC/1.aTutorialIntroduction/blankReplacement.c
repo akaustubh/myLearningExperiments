8f6a05ad08342f5d745f7f1c0de51e5ff2fe5084
akaustubh <akaustubh5@gmail.com>
2022-03-21 07:04:55 +0000

    day 3

---
+++ myExperimentsWithC/1.aTutorialIntroduction/blankReplacement.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define		NONBLANK	'a'
+
+/* replace string of one or more blanks with single blank */
+main()
+{
+	int c, lastc;
+
+	lastc = NONBLANK;
+	while ((c = getchar()) != EOF) {
+		if (c != ' ')
+			putchar(c);
+		if (c == ' ')
+			if (lastc != ' ')
+				putchar(c);
+		lastc = c;
+	}
+}
+
+
